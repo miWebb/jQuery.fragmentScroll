53ab5f1339c19a069c766458cb4726b94598017f
Michael Webbers <m.webbers@miwebb.com>
2014-05-24 21:03:28 +0200

    Initial upload

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 jQuery.fragmentScroll
 =====================
 
-jQuery fragment scroll plugin
+## Example
+
+### Javascript
+
+<pre>$(window).load(function() {
+	$("html, body").fragmentScroll();
+});</pre>
+
+## Options
+
+<pre>$("html, body").fragmentScroll({
+	frame: 'html, body',
+	offset: 0,
+	speed: 1000,
+	easing: 'swing', // http://easings.net
+	showFragment: false
+});</pre>
---
+++ assets/js/jquery.fragmentscroll.js
@@ -0,0 +1,46 @@
+/*!
+ * jQuery Fragment Scroll v1.0
+ *
+ * Copyright 2014 miWebb and other contributors
+ * Released under the MIT license
+ */
+
+(function($) {
+	$.fn.fragmentScroll = function(options) {
+		// Variables
+		var element = this;
+
+		// Options
+		var options = $.extend({
+			frame: 'html, body',
+			offset: 0,
+			speed: 1000,
+			easing: 'swing', // http://easings.net
+			showFragment: false
+		}, options);
+
+		// Init
+		fragmentScroll(element, options);
+	}
+
+	function fragmentScroll(element, options) {
+		// On click event
+		element.find('[href^="#"]').on("click", function(event) {
+			// Prevent default behavior
+			event.preventDefault();
+
+            // Variables
+            var fragment = this.href.substring(this.href.indexOf('#'));
+
+			// Scroll frame
+			$(options.frame).animate({
+				scrollTop: $(fragment).offset().top - $(options.frame).offset().top - options.offset
+			}, options.speed, options.easing);
+
+			// Show fragment
+			if(options.showFragment) {
+				window.location.hash = fragment;
+			}
+		});
+	}
+})(jQuery);
